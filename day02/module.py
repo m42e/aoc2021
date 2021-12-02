e2c1c8c66c89efe39c2bef01672495e1620c9a3e
Matthias Bilger <matthias@bilger.info>
2021-12-02 06:23:55 +0100

    more simple, still silly solution

--- day02/module.py
+++ day02/module.py
@@ -9,35 +9,35 @@ import functools
 
 
 def pw(line):
-    return line.strip()
+    a, b = line.split(" ")
+    b = int(b)
+    return (a, b)
 
 
-m = {"forward": (1, 0), "backwards": (-1, 0), "down": (0, 1), "up": (0, -1)}
+movemap = {"forward": (1, 0), "backwards": (-1, 0), "down": (0, 1), "up": (0, -1)}
+
+
+def add_tuples(a, b, bfactor=1):
+    return (a[0] + b[0] * bfactor, a[1] + b[1] * bfactor)
 
 
 def p1():
     inp = get_input(pw)
-    x, y = (0, 0)
-    for sample in inp:
-        a, b = sample.split(" ")
-        n = m[a]
-        x += n[0] * int(b)
-        y += n[1] * int(b)
-    print(x * y)
+    pos = (0, 0)
+    for a, b in inp:
+        pos = add_tuples(pos, movemap[a], b)
+    print(pos[0] * pos[1])
     return inp
 
 
 def p2(segments):
     inp = get_input(pw)
     x, y = (0, 0)
-    aim = 0
-    for sample in inp:
-        a, b = sample.split(" ")
-        n = m[a]
-        tmp = n[0] * int(b)
-        aim += n[1] * int(b)
-        x += tmp
-        y += tmp * aim
+    movement = (0, 0)
+    for a, b in inp:
+        movement = add_tuples((0, movement[1]), movemap[a], b)
+        x += movement[0]
+        y += movement[0] * movement[1]
 
     print(x * y)
     return inp
