e6dbea15297d0c01fb867ce07d4844299481c93d
Matthias Bilger <matthias@bilger.info>
2021-12-04 06:40:33 +0100

    added nicer print

--- day04/module.py
+++ day04/module.py
@@ -7,16 +7,20 @@ import re
 from aoc.partselector import part_one, part_two
 import functools
 
+
 def pw(line):
     return line.strip()
+
 
 def i(line):
     return int(line.strip())
 
-def ss(line):
-    return pw(line).split(' ')
 
-class Board():
+def ss(line):
+    return pw(line).split(" ")
+
+
+class Board:
     def __init__(self, numbers):
         self.numbers = numbers
         self.crossed = []
@@ -27,24 +31,24 @@ class Board():
             self.crossed.append(a)
 
     def check(self):
-        for i in [0,1,2,3,4]:
-            if all([x in self.crossed for x in range(i,25, 5)]):
+        for i in range(0, 5):
+            if all([x in self.crossed for x in range(i, 25, 5)]):
                 return True
-        for i in [0,5,10,15,20]:
-            if all([x in self.crossed for x in range(i,i+5)]):
+        for i in range(0, 25, 5):
+            if all([x in self.crossed for x in range(i, i + 5)]):
                 return True
-        if all([x in self.crossed for x in [0, 6, 12, 18, 24]]):
-            return True
         return False
+
     def print(self):
-        for i in [0,5,10,15,20]:
-            for x in range(i,i+5):
+        for i in [0, 5, 10, 15, 20]:
+            for x in range(i, i + 5):
                 if x in self.crossed:
-                    print("XX", end=' ')
-                else:
-                    print(f"{self.numbers[x]:02}", end=' ')
+                    print("\033[1m\033[31m", end="")
+                print(f"{self.numbers[x]:02}", end=" ")
+                if x in self.crossed:
+                    print("\033[0m", end="")
             print()
-            print()
+        print()
 
     def sum(self):
         s = 0
@@ -54,11 +58,9 @@ class Board():
         return s
 
 
-
-
 def p1():
     inp = get_input(pw)
-    draw = list(map(int,inp[0].split(',')))
+    draw = list(map(int, inp[0].split(",")))
     inp = inp[1:]
     boards = []
     temp = []
@@ -67,7 +69,7 @@ def p1():
             boards.append(Board(copy.copy(temp)))
             temp = []
         else:
-            for n in list(map(int, map(str.strip, filter(None, sample.split(' '))))):
+            for n in list(map(int, map(str.strip, filter(None, sample.split(" "))))):
                 temp.append(n)
     boards.append(Board(copy.copy(temp)))
 
@@ -75,15 +77,16 @@ def p1():
         for board in boards:
             board.cross(d)
             if board.check():
-                print("components: ", board.sum(),d)
-                print(board.sum()*d)
-                return board.sum()*d
+                board.print()
+                print("components: ", board.sum(), d)
+                print(board.sum() * d)
+                return board.sum() * d
     return inp
 
 
 def p2(segments):
     inp = get_input(pw)
-    draw = list(map(int,inp[0].split(',')))
+    draw = list(map(int, inp[0].split(",")))
     inp = inp[2:]
     boards = []
     temp = []
@@ -92,7 +95,7 @@ def p2(segments):
             boards.append(Board(copy.copy(temp)))
             temp = []
         else:
-            for n in list(map(int, map(str.strip, filter(None, sample.split(' '))))):
+            for n in list(map(int, map(str.strip, filter(None, sample.split(" "))))):
                 temp.append(n)
     boards.append(Board(copy.copy(temp)))
 
@@ -101,30 +104,30 @@ def p2(segments):
         for board in boards:
             board.cross(d)
             if board.check():
-                s+=1
+                s += 1
 
-        if s == len(boards)-1:
+        if s == len(boards) - 1:
             for i, board in enumerate(boards):
                 if board.check() == False:
                     last_winner = i
         if s == len(boards):
+            board.print()
             print("winning board:", last_winner)
-            print("components: ", boards[last_winner].sum() , d)
+            print("components: ", boards[last_winner].sum(), d)
             print(boards[last_winner].sum() * d)
             return 0
 
     return inp
 
+
 result1 = None
 if part_one():
     start = time.time()
     result1 = p1()
-    print(round(1000*(time.time() - start), 2), 'ms')
+    print(round(1000 * (time.time() - start), 2), "ms")
 
 
 if part_two():
     start = time.time()
     p2(result1)
-    print(round(1000*(time.time() - start), 2), 'ms')
-
-
+    print(round(1000 * (time.time() - start), 2), "ms")
