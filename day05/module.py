8f75dc33863dd3ece38cd43d7ea62c4ba46ea36f
Matthias Bilger <matthias@bilger.info>
2021-12-05 06:52:08 +0100

    with image production

--- day05/module.py
+++ day05/module.py
@@ -8,6 +8,7 @@ from aoc.partselector import part_one, part_two
 import functools
 
 max_coords = (0, 0)
+total_max = 0
 def lines(line, diags=False):
     global max_coords
     first, second = line.split(' -> ')
@@ -51,13 +52,27 @@ def draw(points):
                 c.set(x,y)
     print(c.frame())
 
+def image(name, points):
+    from PIL import Image, ImageDraw
+
+    out = Image.new("RGB", (max_coords[0]+1, max_coords[1]+1), (255, 255, 255))
+    draw = out.load()
+    for x in range(0,max_coords[0]):
+        for y in range(0,max_coords[1]):
+            if (x,y) in points:
+                draw[x,y] = (int((255/total_max)*points[(x,y)]), 0, 0)
+    # write to stdout
+    out.save(f"{name}.png", "PNG")
+
 def count(lines):
+    global total_max
     total = 0
     points = collections.defaultdict(int)
     for i, line in enumerate(lines):
         for p in line:
             points[p] +=1
     for p in points.values():
+        total_max = max(total_max, p)
         if p > 1:
             total += 1
 
@@ -65,17 +80,17 @@ def count(lines):
 
 def p1():
     inp = get_input(nodiags)
-    cnt = 0
     cnt, p = count(inp)
     print(cnt)
+    image("1", p)
     return cnt
 
 
 def p2(segments):
     inp = get_input(withdiagonals)
-    cnt = 0
     cnt, p = count(inp)
     print(cnt)
+    image("2", p)
     return cnt
 
 result1 = None
