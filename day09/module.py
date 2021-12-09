097f7eecbbd1e213497e3da73fb6e4e3171a0625
Matthias Bilger <matthias@bilger.info>
2021-12-09 06:41:03 +0100

    day09 python

--- day09/module.py
+++ day09/module.py
@@ -10,15 +10,61 @@ import functools
 def pw(line):
     return line.strip()
 
+def pwi(line):
+    return int(line.strip())
+
+def pwli(line):
+    return list(map(int, line.strip()))
+
 def p1():
     inp = get_input(pw)
-    for sample in inp:
-        print(sample)
-    return inp
+    lowpoints = []
+    coords = []
+    for i, sample in enumerate(inp):
+        for j, c in enumerate(sample):
+            alllow = True
+            if j != 0:
+                alllow = alllow and (int(c) < int(inp[i][j-1]))
+            if i != 0:
+                alllow = alllow and (int(c) < int(inp[i-1][j]))
+            if i+1 < len(inp):
+                alllow = alllow and (int(c) < int(inp[i+1][j]))
+            if j+1 < len(sample):
+                alllow = alllow and (int(c) < int(inp[i][j+1]))
+            if alllow:
+                lowpoints.append(int(c)+1)
+                coords.append((i, j))
+
+    print(sum(lowpoints))
+    return coords
 
 
-def p2(segments):
-    print(len(segments))
+def p2(coords):
+    inp = get_input(pw)
+    dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    walked = {}
+    for c in coords:
+        walked[c] = set([c])
+        walklist = []
+        def addwalk(loc):
+            for d in dirs:
+                if (loc[0] + d[0], loc[1] + d[1]) not in walked[c]:
+                    walklist.append((loc[0] + d[0], loc[1] + d[1], int(inp[loc[0]][loc[1]])))
+
+        addwalk(c)
+        while len(walklist):
+            i,j,v = walklist.pop()
+            if i >= 0 and j>= 0 and i < len(inp) and j < len(inp[0]):
+                if (int(inp[i][j]) < 9):
+                    walked[c].add((i,j))
+                    addwalk((i,j))
+
+    mul=1
+    for v in sorted(walked.items(), key=lambda x: len(x[1]))[-3:]:
+        mul = mul * len(v[1])
+    print(mul)
+
+
     return 0
 
 result1 = None
