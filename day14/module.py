8b9ac873e38b2f3f6abd9368d5205f068e0e3f48
Matthias Bilger <matthias@bilger.info>
2021-12-14 06:55:40 +0100

    day 14

---
+++ day14/module.py
@@ -0,0 +1,102 @@
+from aoc.input import get_input
+import copy
+import itertools
+import time
+import collections
+import re
+from aoc.partselector import part_one, part_two
+import functools
+
+SPLITCHAR = ','
+
+def pw(line):
+    return line.strip()
+
+def pwl(line):
+    return line.strip().split(SPLITCHAR)
+
+def pwi(line):
+    return int(line.strip())
+
+def pwli(line):
+    return list(map(int, line.strip().split(SPLITCHAR)))
+
+def inputpart2(line):
+    return re.match("^$", line) is not None
+
+def p1():
+    inp = get_input(pw, inputpart2)
+    base = inp[0][0]
+    rules = {}
+    for sample in inp[1]:
+        rulepart = sample.split(' -> ')
+        rules[rulepart[0]] = rulepart[1]
+
+    print(base)
+
+    cnts = []
+    knownpattern = {}
+    for _ in range(10):
+        nxt = ''
+        for p in range(0, len(base)-1):
+            if base[p:p+2] in rules:
+                nxt += base[p] + rules[base[p:p+2]]
+            else:
+                nxt += base[p+1]
+        nxt += base[-1]
+        cnt = collections.defaultdict(int)
+        for n in nxt:
+            cnt[n] += 1
+
+        knownpattern = {base:cnt}
+        cnts.append(cnt)
+        base = nxt
+        s = sorted(cnt.items(), key=lambda x: x[1])
+    print(s[-1][1] - s[0][1])
+
+    return inp
+
+
+def p2(ina):
+    inp = get_input(pw, inputpart2)
+    base = inp[0][0] + ' '
+    rules = {}
+    for sample in inp[1]:
+        rulepart = sample.split(' -> ')
+        rules[rulepart[0]] = rulepart[1]
+
+    patterns = collections.defaultdict(int)
+    for p in range(0, len(base)-1):
+        patterns[base[p:p+2]] += 1
+
+    for _ in range(40):
+        npatterns = collections.defaultdict(int)
+        for pattern, pc in patterns.items():
+            if pattern in rules:
+                npatterns[pattern[0] + rules[pattern]] += pc
+                npatterns[rules[pattern] + pattern[1]] += pc
+            else:
+                npatterns[pattern] += pc
+        patterns = npatterns
+
+    cnt = collections.defaultdict(int)
+    for pattern, count in patterns.items():
+        cnt[pattern[0]] += count
+
+    s = sorted(cnt.items(), key=lambda x: x[1])
+    print(s[-1][1] - s[0][1])
+    return inp
+
+result1 = None
+if part_one():
+    start = time.time()
+    result1 = p1()
+    print(round(1000*(time.time() - start), 2), 'ms')
+
+
+if part_two():
+    start = time.time()
+    p2(result1)
+    print(round(1000*(time.time() - start), 2), 'ms')
+
+
